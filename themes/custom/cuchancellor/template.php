60321c09caf8227c57b5aa6f41c70303c6d78e73
Kevin Reynen <kreynen@gmail.com>
2018-05-31 13:44:37 -0600

    removing old custom themes

--- themes/custom/cuchancellor/template.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function cuchancellor_preprocess_html(&$vars) {
-  $vars['classes_array'][]='banner-black';
-  $layout = theme_get_setting('layout_style', 'cuthemed') ? theme_get_setting('layout_style', 'cuthemed') : 'layout-wide';
-  $vars['classes_array'][]=$layout;
-}
